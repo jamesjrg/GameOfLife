4df39f6354a523e06915984a1918fc8e115ae1db
James Gregory <james.jrg@gmail.com>
2014-10-23 16:40:36 +0100

    initial commit

---
+++ GameOfLifeTests/UnitTests.fs
@@ -0,0 +1,16 @@
+﻿namespace GameOfLifeTests
+
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>] 
+type ``Given something or other`` ()=
+    let lightBulb = new LightBulb(true)
+
+    [<Test>] member x.
+     ``when I ask whether it is On it answers true.`` ()=
+            lightBulb.On |> should be True
+
+    [<Test>] member x.
+     ``when I convert it to a string it becomes "On".`` ()=
+            string lightBulb |> should equal "On"
